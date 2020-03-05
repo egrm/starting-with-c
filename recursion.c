b8f9b4fc02c1670754d2619343e42f85b7ece9f4
Egor <egorgromyko@gmail.com>
2020-03-05 06:41:04 +0100

    recursion

---
+++ recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factorial(char n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n-1);
+    }
+}
+
+int main() {
+    /* testing code */
+    printf("1! = %i\n", factorial(1));
+    printf("3! = %i\n", factorial(3));
+    printf("5! = %i\n", factorial(5));
+
+    return 0;
+}
